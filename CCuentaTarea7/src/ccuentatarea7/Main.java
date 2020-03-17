235079d235663baa2e57538cc551d5fd743fe957
Usuario <Usuario@JuanRafael>
2020-03-17 16:31:45 +0100

    “Segunda versión. Refactorizado con método operacuenta y parámetro y encapsulada CCuenta”

--- CCuentaTarea7/src/ccuentatarea7/Main.java
+++ CCuentaTarea7/src/ccuentatarea7/Main.java
@@ -15,13 +15,24 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta cuentaUno;
         double saldoActual;
         
-        miCuenta = new CCuenta("Juan López","1000-2365-85-123456789",2500,0);
-        try 
+        cuentaUno = new CCuenta("Juan López","1000-2365-85-123456789",2500,0);
+        operacionesCuenta(cuentaUno, 20); 
+        saldoActual = cuentaUno.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+    }
+
+    /**
+     *
+     * @param cuentaUno the value of cuentaUno
+     * @param cantidad the value of cantidad
+     */
+    public static void operacionesCuenta(CCuenta cuentaUno, float cantidad) {
+        try
         {
-            miCuenta.retirar(2300);
+            cuentaUno.retirar(2300);
         } catch (Exception e)
         {
             System.out.print("Fallo al retirar");
@@ -30,13 +41,11 @@ public class Main {
         try
         {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            cuentaUno.ingresar(695);
         } catch (Exception e)
         {
             System.out.print("Fallo al ingresar");
         }
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
     }
 
 }
